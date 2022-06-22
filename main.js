b2210cf93ce9c6b690292ecfc38461a1a6082b02
dbdgjthgcbcc <85731247+dbdgjthgcbcc@users.noreply.github.com>
2022-06-22 19:05:36 +1200

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,52 @@
 var SpeechRecognition = window.webkitSpeechRecogntion;
-
+var Content;
 var recognition = new SpeechRecognition();
 
 function start() {
-    document.getElementById("textbox").innerHTML = "";
     recognition.start();
 }
 
 recognition.onresult = function (event) {
     console.log(event);
 
-    var Content = event.results[0][0].transcript;
+    Content = event.results[0][0].transcript.toLowerCase();
     console.log(Content);
+    if (Content == "selfie") {
+        speak();
+    }
 
-    document.getElementById("textbox").innerHTML = Content;
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    Webcam.attch(camera);
+
+    speak_data = "Taking your next Selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+
+    setTimeout(function () {
+        img_id = "selfie1";
+        take_snapshot();
+        speak_data = "Taking your next selfie in 10 seconds";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+
+    }, 5000);
+
+    setTimeout(function () {
+        img_id = "selfie2";
+        take_snapshot();
+        speak_data = "Taking your next selfie in 15 seconds";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+
+    }, 10000);
+    
+    setTimeout(function () {
+        img_id = "selfie3";
+        take_snapshot();
+
+    }, 15000)
 }
